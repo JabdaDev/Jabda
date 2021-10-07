4b0b0bb16003401c0daa776fdc42f4945a77e318
JabdaDev <92113070+JabdaDev@users.noreply.github.com>
2021-10-08 02:17:34 +0900

    잡다한 git First Test

---
+++ jabda/src/main/java/com/example/demo/JabdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabdaApplication.class, args);
+	}
+
+}
